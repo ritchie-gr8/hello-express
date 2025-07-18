4b012c1716383bb7055ace8e1b9621b442245497
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-07-18 16:32:28 +0700

    fix: bind to process.env.PORT for Dokku compatibility

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors());
